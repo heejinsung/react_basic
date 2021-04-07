849ad2f02539a5e6c058db87e5dffc10670ed867
heejin <shj683706@gmail.com>
2021-04-07 16:01:44 +0900

    navigation/detail design

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,31 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import "./Movie.css";
+import { Link } from 'react-router-dom';
 
-function Movie({title, year, summary, poster, genres}){
-   return (
-   <div className="movie">
-      <img src={poster} alt={title} onError={(e)=>{e.target.onerror = null; e.target.src="https://developers.google.com/maps/documentation/streetview/images/error-image-generic.png"}}/>
-      <div className="movie_data">
-         <h3 className="movie_title">{title}</h3>
-         <h5 className="movie_year">{year}</h5>
-         <ul>
-            {genres.map((genre, idx) => {
-               return <li key={idx}>[{genre}]</li>;
-            })}
-         </ul>
-         <p className="movie_summary">{summary.slice(0,90)}...</p>
+function Movie({ title, year, summary, poster, genres }) {
+  return (
+      <div className='movie'>
+        <Link to={{ pathname: "/movie_detail", state: { year, title, summary, poster, genres } }}>
+         <div className='movie_inside'>
+            <img
+               src={poster}
+               alt={title}
+               onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src =
+                  "https://developers.google.com/maps/documentation/streetview/images/error-image-generic.png";
+               }}
+            />
+            <div className='movie_data'>
+               <h3 className='movie_title'>{title.slice(0, 15)}...</h3>
+               <h5 className='movie_year'>{year}</h5>
+               <ul>
+                  {/* {genres.map((genre) => (
+                     <li>{genre}</li>
+                  ))} */}
+                  {genres.map((genre, idx) => {
+                  return <li key={idx}>[{genre}]</li>;
+                  })}
+               </ul>
+               <p className='movie_summary'>{summary.slice(0, 100)}...</p>
+            </div>
+         </div>
+         </Link>
       </div>
-      
-   </div>
-   )
+  );
 }
+
 Movie.propType = {
-   title : PropTypes.string.isRequired,
-   year : PropTypes.number.isRequired,
-   summary : PropTypes.string.isRequired,
-   poster : PropTypes.string.isRequired,
-   genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
